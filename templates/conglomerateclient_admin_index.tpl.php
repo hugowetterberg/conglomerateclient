27054d378174a04e38418d42cb0c18de4d9f2a35
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2009-11-10 13:14:11 +0100

    It's now possible to add conglomerate servers

--- templates/conglomerateclient_admin_index.tpl.php
+++ templates/conglomerateclient_admin_index.tpl.php
@@ -3,6 +3,9 @@
 ?>
 <div class="conglomerateclient-index">
   <ul class="conglomerate-server-list">
+    <?php if (empty($servers)): ?>
+      <li class="no-servers"><?php print t('No servers added'); ?></li>
+    <?php endif ?>
     <?php foreach ($servers as $sid => $info): ?>
       <li class="server"><?php print l($info['name'], 'admin/conglomerateclient/servers/' . $sid) ?></li>
     <?php endforeach ?>
