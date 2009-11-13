4c5d8ea14ee87548d4125acc4e493f8b5052c51c
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2009-11-13 08:23:43 +0100

    Adding a authorization link

--- templates/conglomerateclient_admin_index.tpl.php
+++ templates/conglomerateclient_admin_index.tpl.php
@@ -7,7 +7,15 @@
       <li class="no-servers"><?php print t('No servers added'); ?></li>
     <?php endif ?>
     <?php foreach ($servers as $sid => $info): ?>
-      <li class="server"><?php print l($info['name'], 'admin/conglomerateclient/servers/' . $sid) ?></li>
+      <li class="server">
+        <div class="server-name"><?php print l($info['name'], 'admin/conglomerateclient/servers/' . $sid) ?></div>
+        <a class="address"><?php print l($info['url'], $info['url']); ?></a>
+        <?php if (!empty($info['oauth_token_key'])): ?>
+          <span class="authorization authorized"><?php print t('Authorized'); ?></span>
+        <?php else: ?>
+          <?php print l(t('Authorize'), sprintf('admin/settings/conglomerateclient/authorize/%d', $info['sid'])); ?>
+        <?php endif ?>
+      </li>
     <?php endforeach ?>
   </ul>
   <div class="conglomerateclient-messages">
